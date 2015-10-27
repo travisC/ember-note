591c35f40e5abdbabffcabdd463be0c881485bd6
Travis Cunningham <travis@travis-cunningham.com>
2015-10-26 17:12:38 -0700

    chapter 4 flair

--- app/components/edit-note/component.js
+++ app/components/edit-note/component.js
@@ -1,9 +1,9 @@
 /***
  * Excerpted from "Deliver Audacious Web Apps with Ember 2",
  * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
+ * Copyrights apply to this code. It may not be used to create training material,
  * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
+ * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/mwjsember for more book information.
 ***/
 import Ember from 'ember';
@@ -12,6 +12,7 @@ export default Ember.Component.extend({
   actions: {
     saveNote: function() {
       this.get('note').save();
+      this.sendAction('close');
     },
     closeNote: function() {
       this.sendAction('close');
